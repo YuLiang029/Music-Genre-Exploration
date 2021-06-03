9bfda6380afc5344bab61de81c22167587396d78
Yu Liang <y.liang-2@outlook.com>
2021-06-03 15:11:36 +0200

    add environment files for storing secrete keys

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if test -f ".env" then
+  export $(cat .env | xargs)
+fi
+
+if [ -z "$POSTGRESQL_PASSWORD"]; then "You forgot to set POSTGRESQL_PASSWORD variable!" && exit 1; fi
+
 print $YU_SPOTIFY_CLIENT_ID

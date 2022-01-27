e7e2aff0277c9c12d2c0478160bd58602c26bf4c
Yu Liang <y.liang-2@outlook.com>
2022-01-27 17:02:54 +0100

    heroku setup

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'test.db').replace("mysql://", "mysql+pymysql://")
 
 # Database configuration for upgraded HEROKU database
-# SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_MAROON_URL').replace("://", "ql://", 1)
+SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_MAROON_URL').replace("://", "ql://", 1)
 # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
 # Database for postgresql
@@ -23,9 +23,9 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 #                                          'postgresql://localhost/dbdw').replace(
 #                                          "mysql://", "mysql+pymysql://")
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
-                                         'postgresql://localhost/long_copy').replace(
-                                         "mysql://", "mysql+pymysql://")
+# SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
+#                                          'postgresql://localhost/long_copy').replace(
+#                                          "mysql://", "mysql+pymysql://")
 
 DATABASE_CONNECT_OPTIONS = {}
 
